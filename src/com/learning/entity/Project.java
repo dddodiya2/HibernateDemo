cb9b399a7c56a50130b518703c30295eb4e24dee
dddodiya2 <dddodiya2@gmail.com>
2022-06-28 18:13:53 +0530

    Lazy and Eager Loading

---
+++ src/com/learning/entity/Project.java
@@ -0,0 +1,68 @@
+package com.learning.entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Project {
+
+	@Id
+	@Column
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column(unique = true, name = "title")
+	private String title;
+
+	// private int student_id;
+
+	@ManyToOne(fetch = FetchType.EAGER, cascade = { CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH })
+	@JoinColumn(name = "student_id")
+	private StudentEntity student;
+
+	public Project() {
+	}
+	
+	public Project(String title) {
+		this.title = title;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public StudentEntity getStudent() {
+		return student;
+	}
+
+	public void setStudent(StudentEntity student) {
+		this.student = student;
+	}
+
+	@Override
+	public String toString() {
+		return "Project [id=" + id + ", title=" + title + ", student=" + student.getName() + "]";
+	}
+
+}
